1721fec05e499bdd1d39fb95498a7a3583d3465c
PYGAME <vdk45blg@gmail.co>
2022-08-18 14:26:27 +0900

    Пользовательские теги

--- science_restaurant/controllers/templatetags/controllers_tags.py
+++ science_restaurant/controllers/templatetags/controllers_tags.py
@@ -1,2 +1,25 @@
 from django import template
 from controllers.models import *
+
+
+register = template.Library()
+
+
+@register.simple_tag(name='get_cats')
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
+
+
+@register.inclusion_tag('restaurant/list_categories.html')
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+
+    return {"cats": cats, "cat_selected": cat_selected}
+
+
